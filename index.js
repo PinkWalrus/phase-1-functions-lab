5ba1dc322c74947ae6eabe5f464c711839522674
Loren <lorensprague@icloud.com>
2022-07-15 10:39:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42;
+    } else {
+        return 42 - blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, stop) {
+    if (start < stop) {
+        return (stop - start) * 264;
+    } else {
+        return (start - stop) * 264;
+    }
+}
+
+function calculatesFarePrice(start, stop) {
+    const distance = distanceTravelledInFeet(start, stop);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance >= 400 && distance <= 2000) {
+        return (distance - 400) * .02;
+    } else if (distance >= 2000 && distance <= 2500) {
+        return 25;
+    } else (distance); {
+        return 'cannot travel that far';
+    }
+}
